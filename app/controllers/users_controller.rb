11378274a7ce34753f68c9f58442fcc092b4d900
root <root@sayu2308>
2023-11-01 22:34:40 +0900

    年齢の初期値および順番

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.where(age: 20..50).order(age: :desc)
   end
 
   # GET /users/1 or /users/1.json
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @user.name = 'らんてくん'
+    @user.age = 20
   end
 
   # GET /users/1/edit
